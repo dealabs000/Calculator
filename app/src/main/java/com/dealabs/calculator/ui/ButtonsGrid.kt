12a1e45bcd8541ab69675b9e2023376b547fb224
Your Name <you@example.com>
2026-07-18 13:57:32 +0100

    Initial commit - APK build setup 2026-07-18 13:57:31

--- app/src/main/java/com/dealabs/calculator/ui/ButtonsGrid.kt
+++ app/src/main/java/com/dealabs/calculator/ui/ButtonsGrid.kt
@@ -1,9 +1,9 @@
 package com.dealabs.calculator.ui
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
